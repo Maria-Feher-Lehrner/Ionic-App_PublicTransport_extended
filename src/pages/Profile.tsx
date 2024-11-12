f4114a8d37cd4a6adfdc0e8314cd6ca7f9aace4a
Maria Feher-Lehrner <maria@feher.at>
2024-11-12 14:23:04 +0100

    feature: added basic app- and developer information to profile page including basic css

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,15 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import packageJson from '../../package.json';
 import './Profile.css';
+import '../theme/main.css';
+import MapContainer from "../components/MapContainer";
+
+const appVersion = packageJson.version;
+const appName = packageJson.name;
+const appDescription = packageJson.description;
+const developerName = "Maria Feher-Lehrner";
+const developerAddress = "Bringamosa 2-Z, Santa Cruz, Aruba";
+const developerLocation = "";
 
 const Profile: React.FC = () => {
   return (
@@ -16,7 +25,15 @@ const Profile: React.FC = () => {
             <IonTitle size="large">Profile</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Profile page"/>
+          <div className="content-section">
+          <h1>About this App</h1>
+          <p>{appDescription}</p>
+          <p><strong>App name:</strong> {appName}</p>
+          <p><strong>Version:</strong> {appVersion}</p>
+          <p><strong>Developed by:</strong> {developerName}</p>
+          <p><strong>Address:</strong> {developerAddress}</p>
+          </div>
+        <MapContainer name="Find me here"/>
       </IonContent>
     </IonPage>
   );
